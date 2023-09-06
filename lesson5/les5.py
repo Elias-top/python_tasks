322deb770f6ae9f4abab8bcc87524b45863a3e29
«Ilya» <bkmz1999ilyxa@gmail.com>
2023-09-06 23:56:30 +0300

    Сделана 32 задача 5 семинара

--- lesson5/les5.py
+++ lesson5/les5.py
@@ -15,3 +15,28 @@
 # num_of_elem = int(input("Введи колличество элементов прогрессии"))
 
 # print(*get_arefm_prog(first_elem, difference, num_of_elem))
+
+# Задача 32: Определить индексы элементов массива (списка),значения которых принадлежат заданному диапазону [5 : 15](т.е. не
+# меньше заданного минимума и не больше заданного максимума)
+
+# import random
+
+# def get_specific_index_from_random_array(array, first_num_range, second_num_range):
+#     temp_array = []
+#     temp_index = 0
+#     for x in array:
+#         if(x >= 5 and x <= 15):
+#             temp_array.append(temp_index)
+#         temp_index += 1
+#     return temp_array
+            
+
+
+# rand_array = []
+# for x in range(0, 15):
+#     rand_array.append(random.randint(-15, 30))
+
+# print(rand_array)
+# print(get_specific_index_from_random_array(rand_array, 5, 15))
+
+
