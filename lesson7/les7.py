5c164a1231078d9f4612d6ee7a9b4b9583a1d58b
«Ilya» <bkmz1999ilyxa@gmail.com>
2023-09-11 21:42:47 +0300

    сделана 36 задача 7 урока

--- lesson7/les7.py
+++ lesson7/les7.py
@@ -29,3 +29,19 @@
 #     print("Пам парам")
 
 
+# Задача 36: Напишите функцию print_operation_table(operation, num_rows=6, num_columns=6),
+# которая принимает в качестве аргумента функцию, вычисляющую элемент по номеру строки и
+# столбца. Аргументы num_rows и num_columns указывают число строк и столбцов таблицы,
+# которые должны быть распечатаны. Нумерация строк и столбцов идет с единицы (подумайте,
+# почему не с нуля). Примечание: бинарной операцией называется любая операция, у которой
+# ровно два аргумента, как, например, у операции умножения.
+
+# def print_operation_table(temp_func, num_rows, num_colums):
+#     for x in range(1, num_rows + 1):
+#         for y in range(1, num_colums + 1):
+#             print(temp_func(x,y), end='\t')
+#         print()
+
+# num_row = int(input("Введите количество строк: "))
+# num_colums = int(input("Введите количество столбцов: "))
+# print_operation_table(lambda x,y: x * y, num_row, num_colums)
